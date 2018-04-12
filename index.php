930ea212d5f748869d51e07f7f7bcb017cefe68a
Danilo Miranda <nilomiranda3@gmail.com>
2018-04-12 11:51:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -227,8 +227,8 @@ if(isset($_POST['name']) && isset($_POST['email'])) {
       $mail->isSMTP();                                      // Set mailer to use SMTP
       $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
       $mail->SMTPAuth = true;                               // Enable SMTP authentication
-      $mail->Username = 'tqsmedium@gmail.com';                 // SMTP username
-      $mail->Password = 'QueensSpeech2015';                           // SMTP password
+      $mail->Username = '#####';                 // SMTP username
+      $mail->Password = '#####';                           // SMTP password
       $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
       $mail->Port = 587;                                    // TCP port to connect to
 
